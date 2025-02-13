efb9ca54cf7364f6a8451fc7b7f510010e7244dd
Vaishnavi-arjun-1905 <vaishnavia600@gmail.com>
2025-02-13 23:34:51 +0530

    using simple if to check two numbers are equal

---
+++ two equal numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+   
+     int a;
+     int b;
+     
+     printf("Enter the 1st number\n");
+     scanf("%d", &a);
+     
+     printf("Enter the 2nd number\n");
+     scanf("%d", &b);
+     
+     if(a==b)
+     {
+         printf("Both the numbers are equal\n");
+     }
+     printf("condition checked\n");
+     
+    return 0;
+}
